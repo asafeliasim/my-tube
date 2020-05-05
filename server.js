e8165ecd8021301d5637a4e1813cd967c3be886c
asafeliasim <asafeliasim@gmail.com>
2020-05-05 17:56:41 +0300

    connect mongo and desing user route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Connect to DB
+connectDB();
 
 app.get('/',(req,res)=>res.json({msg: 'Welcome to MY-TUBE API'}));
+
+// Init Middleware
+app.use(express.json({extended:false}));
 
 // Define Routes
 app.use('/api/users',require('./routes/users'));
