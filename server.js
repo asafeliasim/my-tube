0b64c3d64942acc86dcc678f43137840ef7f21f9
asafeliasim <asafeliasim@gmail.com>
2020-05-09 09:58:08 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +19,13 @@ app.use(express.json());
 app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/playlist',require('./routes/platlists'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
+
 app.listen(PORT,()=>{
     console.log('Server started on port '+ PORT);
 });
